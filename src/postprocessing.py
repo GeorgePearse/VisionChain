d31c6674e4da16503cb58ce07fc21520334e702a
George Pearse <georgepearse@georges-air-2.home>
2023-07-13 21:00:37 +0100

    bit stuck on shape filter

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -11,6 +11,30 @@ class PostprocessingHook:
 
 @dataclass
 class Thresholding(PostprocessingHook): 
-
+    """
+    """
     def run(self, predictions: Predictions) -> Predictions:
 
+
+@dataclass
+class ShapeFilter(PostprocessingHook): 
+    class_name: str
+    min_width: int = None
+    max_width: int = None 
+    min_height: int = None 
+    max_height: int = None
+
+    def run(self, predictions: Predictions) -> Predictions: 
+        
+
+
+@dataclass 
+class Postprocessor: 
+    hooks: List[PostprocessingHook] = None
+    
+    def run(self, predictions: Predictions) -> Predictions:
+        
+        for hook in self.hooks: 
+            predictions = hook.run(predictions)
+
+        return predictions
