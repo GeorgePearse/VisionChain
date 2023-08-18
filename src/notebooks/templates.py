0e124bc7b65340d7825f7ca011ab76476109db25
Ubuntu <ubuntu@ip-172-31-12-159.ec2.internal>
2023-08-18 19:21:26 +0000

    maybe getting somewhere with templates

---
+++ src/notebooks/templates.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+from PIL import Image
+
+@dataclass
+class Image: 
+    """
+    To prevent the system from having to open the 
+    file multiple times. 
+
+    Pass this object around, 
+    """
+    file_path: str
+    image: PIL.Image = None
+
+    def open(self): 
+        """
+        To prevent models in the same pipeline
+        having to reopen the same file. 
+
+        Wherever possible.
+        """
+        if self.image is not None: 
+            return image
+
+        else: 
+            self.image = Image.open(file_path)
+            return self.image
+
+
+@dataclass
+class ObjectCropper:
+    path_to_weights: str
+    threshold: float
+
+    """
+    Just a model that's good at finding objects 
+    something like SAM or GroundingDino.
+    """
+    def predict() -> Detections:
+        ...
+
+
+@dataclass
+class ObjectClassifier:
+    path_to_weights: str
+    thresholds: dict
+
+    def predict(image_path, preds: Detections) -> Detections:
+        for pred in preds: 
+           
+
+@dataclass
+class TwoStageDetector: 
+    """
+    Model consisting of one cropper and one 
+    classifier.
+    """
+    object_cropper: ObjectCropper
+    object_classifier: ObjectClassifier
+
+    def predict(image_path: str) -> Detections:
+        
+
+@datclass 
+class RegionOfInterestFilterer: 
+    """
+    Only count the detections within 
+    this space 
+
+    (use supervision to achieve that).
+
+    Can be run before or after the Detector.
+    """
+    path_to_weights: str 
+    threshold: float 
+
+    def predict(image, preds: Detections) -> Detections: 
